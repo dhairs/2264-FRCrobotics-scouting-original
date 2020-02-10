a0393aac0b55cff420020a66265b5cdd4361221d
DhairyaG23 <dhairyag23@gmail.com>
2020-02-09 23:25:09 -0600

    dd

---
+++ main/js/index.js
@@ -0,0 +1,37 @@
+var serverStatusRequest = new XMLHttpRequest();
+var nameRequest = new XMLHttpRequest();
+
+var serverStatus;
+
+serverStatusRequest.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    
+    var serverRequest = JSON.parse(this.responseText);
+
+    if(serverRequest.is_datafeed_down == false){
+        serverStatus = "Active";
+        document.getElementById('server-status').style.backgroundColor = "rgb(50, 168, 82)";
+    } else {
+        serverStatus = "Down"
+        document.getElementById('server-status').style.backgroundColor = "rgb(184, 26, 26)";
+    }
+    document.getElementById("server-status").innerHTML = serverStatus;
+  }
+};
+
+nameRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200){
+        
+        var nameRequestObj = JSON.parse(this.responseText); 
+
+        console.log(nameRequestObj.nickname);
+    }
+}
+
+serverStatusRequest.open("GET", "https://www.thebluealliance.com/api/v3/status", true);
+serverStatusRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
+serverStatusRequest.send();
+
+nameRequest.open("GET", "https://www.thebluealliance.com/api/v3/teams/1", true);
+nameRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
+nameRequest.send();

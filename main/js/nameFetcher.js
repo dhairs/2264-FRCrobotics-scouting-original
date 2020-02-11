da97e4dbf04f4250c724c9f387a966e307ad8a1f
ccans <55026208+ccans@users.noreply.github.com>
2020-02-10 22:23:01 -0600

    jjjhkjh

--- main/js/nameFetcher.js
+++ main/js/nameFetcher.js
@@ -4,6 +4,7 @@ var nameRequest = new XMLHttpRequest();
 var serverStatus;
 
 
+
 nameRequest.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200){
         var nameArray = [];
@@ -12,36 +13,24 @@ nameRequest.onreadystatechange = function() {
         for (i = 0; i < nameRequestObj.length; i++) {
         console.log(nameRequestObj[i].nickname);
         nameArray.push(nameRequestObj[i].nickname);
-        console.log(nameArray);
       }
-
-
-      // Make a container element for the list
-    listContainer = document.createElement('div'),
-    // Make the list
-    listElement = document.createElement('ul'),
-    // Set up a loop that goes through the items in listItems one at a time
-    numberOfListItems = nameArray.length,
-    listItem,
-    e;
-
-    // Add it to the page
-    document.getElementsByTagName('body')[0].appendChild(listContainer);
-    listContainer.appendChild(listElement);
-
-    for (e = 0; e < numberOfListItems; ++e) {
-        // create an item for each one
-        listItem = document.createElement('li');
-
-        // Add the item text
-        listItem.innerHTML = nameArray[e];
-
-        // Add listItem to the listElement
-        listElement.appendChild();
-
+      console.log(nameArray);
     }
+
+
+
+    ul = document.createElement('ul');
+
+    document.getElementById('myItemList').appendChild(ul);
+
+    nameArray.forEach(function (name) {
+        let li = document.createElement('li');
+        ul.appendChild(li);
+
+        li.innerHTML += name;
+    });
 }
-}
+
 
 nameRequest.open("GET", "https://www.thebluealliance.com/api/v3/teams/1", true);
 nameRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");

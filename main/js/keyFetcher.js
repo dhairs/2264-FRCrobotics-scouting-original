5b16f86c31ad831b9780a565210907026f7c6a75
ccans <55026208+ccans@users.noreply.github.com>
2020-02-11 22:19:38 -0600

    everything back to normal, for loop still causing trouble

--- main/js/keyFetcher.js
+++ main/js/keyFetcher.js
@@ -1,11 +1,14 @@
 var keyRequest = new XMLHttpRequest();
 var ekeyRequest = new XMLHttpRequest();
+let enameRequest = new XMLHttpRequest();
 
 var keyArray = [];
 var ekeyArray = [];
-var page;
-page = 1;
-
+var enameArray = [];
+var done = false;
+var p;
+var c = 0;
+var enameRequestObj
 
 keyRequest.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200){
@@ -16,16 +19,25 @@ keyRequest.onreadystatechange = function() {
 
 
         for (a = 0; a < keyRequestObj.length; a++) {
-        console.log(keyRequestObj[a].key);
+        // console.log(keyRequestObj[a].key);
         keyArray.push(keyRequestObj[a].key);
       }
-      console.log(keyArray);
+      // console.log(keyArray);
     }
 }
 
-ekeyRequest.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200){
+// enameRequest.onreadystatechange = function() {
+//
+//
+//
+// }
 
+
+
+
+
+ekeyRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
 
         var ekeyRequestObj = JSON.parse(this.responseText);
         var b;
@@ -33,16 +45,42 @@ ekeyRequest.onreadystatechange = function() {
         for(b = 0; b < ekeyRequestObj.length; b++) {
         // console.log(ekeyRequestObj[b]);
         ekeyArray.push(ekeyRequestObj[b]);
+
       }
-      // console.log(ekeyArray);
-    }
+        for(c = 0; c < 100; c++) {
+          enameRequest.open('GET', "https://www.thebluealliance.com/api/v3/event/" + ekeyArray[c] + "/simple", true);
+          enameRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
+          enameRequest.send();
+          // enameRequestObj = enameRequest.responseText
+          // console.log(enameRequestObj);
+
+          }
+        }
+
+      }
+
+
+enameRequest.onreadystatechange = function() {
+  if (enameRequest.readyState == 4 && enameRequest.status == 200) {
+    console.log("ok");
+    var enameRequestObj = enameRequest.responseText
+    console.log(enameRequestObj);
+
+  }
 }
 
-keyRequest.open("GET", "https://www.thebluealliance.com/api/v3/teams/" + page , true);
+
+
+
+// enameRequest.open("GET", "https://www.thebluealliance.com/api/v3/event/", true);
+// enameRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
+// enameRequest.send();
+
+
+
+keyRequest.open("GET", "https://www.thebluealliance.com/api/v3/teams/1" , true);
 keyRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
 keyRequest.send();
-
-
 
 
 ekeyRequest.open("GET", "https://www.thebluealliance.com/api/v3/events/2019/keys", true);
--- main/js/nameFetcher.js
+++ main/js/nameFetcher.js
@@ -3,18 +3,8 @@ var nameRequest = new XMLHttpRequest();
 
 var serverStatus;
 
-var page;
-
-function changePage(page){
-  nameRequest.open("GET", "https://www.thebluealliance.com/api/v3/teams/" + page, true);
-  nameRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
-  nameRequest.send();
-}
-
 nameRequest.onreadystatechange = function() {
-  nameRequest.open("GET", "https://www.thebluealliance.com/api/v3/teams/" + 1, true);
-  nameRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
-  nameRequest.send();
+
     if (this.readyState == 4 && this.status == 200){
 
         var nameArray = [];
@@ -24,7 +14,6 @@ nameRequest.onreadystatechange = function() {
           for (i = 0; i < nameRequestObj.length; i++) {
 
           nameArray.push(nameRequestObj[i].nickname);
-          changePage(page);
 
 
         }
@@ -43,12 +32,6 @@ nameRequest.onreadystatechange = function() {
     });
 }
 
-// function pageChange(){
-//   for(page = 0; page < 10; page++) {
-//
-//   }
-// }
-
-// nameRequest.open("GET", "https://www.thebluealliance.com/api/v3/teams/" + page, true);
-// nameRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
-// nameRequest.send();
+nameRequest.open("GET", "https://www.thebluealliance.com/api/v3/teams/1", true);
+nameRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
+nameRequest.send();

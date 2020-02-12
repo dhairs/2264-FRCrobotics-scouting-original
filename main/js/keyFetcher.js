08aaadec4b9770b2ced26a9b62386138f4c6f3a8
ccans <55026208+ccans@users.noreply.github.com>
2020-02-11 23:27:35 -0600

    Stuff works really well now

--- main/js/keyFetcher.js
+++ main/js/keyFetcher.js
@@ -1,47 +1,34 @@
+// Declaring server requests
 var keyRequest = new XMLHttpRequest();
-var ekeyRequest = new XMLHttpRequest();
 let enameRequest = new XMLHttpRequest();
 
+//Declaring global varibales
 var keyArray = [];
 var ekeyArray = [];
 var enameArray = [];
 var done = false;
 var p;
 var c = 0;
+var b;
 var enameRequestObj
 
+//This function gets the keys of the first 500 teams (Will make it get all keys later)
 keyRequest.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200){
-
 
         var keyRequestObj = JSON.parse(this.responseText);
         var a;
 
-
         for (a = 0; a < keyRequestObj.length; a++) {
-        // console.log(keyRequestObj[a].key);
+
         keyArray.push(keyRequestObj[a].key);
       }
-      // console.log(keyArray);
+
     }
 }
 
+//This function makes 2 arrays, one with event keys and one with event names.
 
-
-ekeyRequest.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-
-        var ekeyRequestObj = JSON.parse(this.responseText);
-        var b;
-
-        for(b = 0; b < ekeyRequestObj.length; b++) {
-        // console.log(ekeyRequestObj[b]);
-        ekeyArray.push(ekeyRequestObj[b]);
-
-      }
-      }
-    }
-var b;
 enameRequest.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 
@@ -51,17 +38,14 @@ enameRequest.onreadystatechange = function() {
         for(c = 0; c < enameRequestObj.length; c++) {
 
         enameArray.push(enameRequestObj[c].name);
-        enameArray.push(enameRequestObj[c].key);
+        ekeyArray.push(enameRequestObj[c].key);
       }
       console.log(enameArray);
+      console.log(ekeyArray);
     }
 }
 
-
-
-
-
-
+//API stuff
 enameRequest.open('GET', "https://www.thebluealliance.com/api/v3/events/2020", true);
 enameRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
 enameRequest.send();
@@ -69,8 +53,3 @@ enameRequest.send();
 keyRequest.open("GET", "https://www.thebluealliance.com/api/v3/teams/1" , true);
 keyRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
 keyRequest.send();
-
-
-ekeyRequest.open("GET", "https://www.thebluealliance.com/api/v3/events/2019/keys", true);
-ekeyRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
-ekeyRequest.send();

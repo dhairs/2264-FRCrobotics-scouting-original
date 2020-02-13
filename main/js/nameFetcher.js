094c11d118e8e836bfa6e5f05339d05b95e29586
ccans <55026208+ccans@users.noreply.github.com>
2020-02-12 18:43:29 -0600

    CSS fixed, js can now modify html classes

--- main/js/nameFetcher.js
+++ main/js/nameFetcher.js
@@ -26,6 +26,8 @@ nameRequest.onreadystatechange = function() {
 
     nameArray.forEach(function (name) {
         let li = document.createElement('li');
+        ul.classList.add('listStuff');
+        li.classList.toggle('inline-centering');
         ul.appendChild(li);
 
         li.innerHTML += name;

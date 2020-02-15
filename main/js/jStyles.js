d21bebc7082809e6e46595d8609ff248cfb1cd29
Dhairya Gupta <dhairyag23@gmail.com>
2020-02-14 20:03:28 -0600

    okurrr

--- main/js/jStyles.js
+++ main/js/jStyles.js
@@ -1,11 +1,15 @@
 function main(){
   $('.massive-container').hide();
   $('form').hide();
+
   $('.massive-container').fadeIn(2500);
   setTimeout(function () {
+
     $('form').fadeIn(2000);
   }, 300);
+  
   $('form').show();
-  $('.massive-container').fadeIn(2500);
+
+  $('.massive-container').show();
 }
 $(document).ready(main);

7f1f41e1ae9e71d893cf68be6706938424cd7cae
DhairyaG23 <dhairyag23@gmail.com>
2020-02-15 19:07:11 -0600

    nvm that code didnt work this is new working js cookie code AGAIn

--- main/js/myTeam.js
+++ main/js/myTeam.js
@@ -1,3 +1,10 @@
+
+var date = new Date();
+date.setTime(date.getTime() + (2592000));
+
 function createCookie(value) {
-    $.cookie('teamID', value, { expires: 14 });  // expires after 14 days
+    var cookie = "teamID=" + value + ";";
+    document.cookie = cookie;
+    console.log(cookie);
+    console.log("Creating new cookie with key: teamID value: " + value);
 }

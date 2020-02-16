0208c84d31e2b927933042c5fda66cc244799267
Ritesh Kumar Mangal (HEALTH) <ritesma@wipro.com>
2020-02-16 07:38:43 -0600

    ohmagah

--- main/js/scoreFetcher.js
+++ main/js/scoreFetcher.js
@@ -1,6 +1,8 @@
 var tKey;
 var ekey;
-function getTeamScores(tKey, eKey) {
+// getTeamScores();
+var blueKeyArray;
+function getTeamScores() {
   var teamScoreRequest = new XMLHttpRequest();
   teamScoreRequest.open("GET", "https://www.thebluealliance.com/api/v3/team/" + "frc1482" + "/event/" + "2019abca" + "/matches/simple" , true);
   teamScoreRequest.setRequestHeader("X-TBA-Auth-Key", "lrqZK0XAvSpeHXuWi9vhbmnAbF4ueBRQB3OevJC1pOWIWQdwX1WKRJ4oQceP0ox5");
@@ -8,7 +10,11 @@ function getTeamScores(tKey, eKey) {
   teamScoreRequest.onreadystatechange = function() {
     var teamScoreRequestObj = JSON.parse(this.responseText);
     var j;
-    console.log(teamScoreRequestObj.team_keys);
+    for (j = 0; j < teamScoreRequestObj.length; j++) {
+      for(b = 0,) {}
+    blueKeyArray = teamScoreRequestObj[a].alliances.blue.team_keys;
+  }
+  }
 
   }
 

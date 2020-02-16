601b4b2263968ad527f782d555077889b9b2b3e1
DhairyaG23 <dhairyag23@gmail.com>
2020-02-15 19:26:37 -0600

    trying to expire on certain time

--- main/js/myTeam.js
+++ main/js/myTeam.js
@@ -3,8 +3,14 @@ var date = new Date();
 date.setTime(date.getTime() + (2592000));
 
 function createCookie(value) {
-    var cookie = "teamID=" + value + ";";
+    var cookie = "teamID=" + value + ";expires=" + date.toGMTString()+";" ;
     document.cookie = cookie;
     console.log(cookie);
     console.log("Creating new cookie with key: teamID value: " + value);
 }
+
+function checkCookie(){
+
+}
+
+$(document).ready(checkCookie());

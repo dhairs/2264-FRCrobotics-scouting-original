e190e482ea5013b864b2c4677e666f365beacfa1
DhairyaG23 <dhairyag23@gmail.com>
2020-02-15 19:45:02 -0600

    Update myTeam.js

--- main/js/myTeam.js
+++ main/js/myTeam.js
@@ -1,7 +1,7 @@
 
 var date = new Date();
-date.setTime(date.getTime() + (2592000));
-
+date.setTime(date.getTime() + (1000*60*60*24*30));
+//date.setTime(date.getTime() + (2592000000));
 function createCookie(value) {
     var cookie = "teamID=" + value + ";expires=" + date.toGMTString()+";" ;
     document.cookie = cookie;
@@ -10,7 +10,23 @@ function createCookie(value) {
 }
 
 function checkCookie(){
-
+    let key = "teamID=";
+            let cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                let cookie = cookies[i];
+                console.log(cookie)
+                while (cookie.charAt(0) === ' ') {
+                    cookie = cookie.substring(1, cookie.length);
+                }
+                if (cookie.indexOf(key) === 0) {
+                    return cookie.substring(key.length, cookie.length);
+                }
+            }
+            return null;
 }
 
+ function deleteCookie() {
+        createCookie("teamID", "", -1);
+    }
+	
 $(document).ready(checkCookie());

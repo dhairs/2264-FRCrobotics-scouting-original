1ebac2d747d3b5d0a9b2e0006d7efa5f76b7c9d0
ccans <55026208+ccans@users.noreply.github.com>
2020-02-16 09:17:53 -0600

    score works?
    score works

--- main/js/scoreFetcher.js
+++ main/js/scoreFetcher.js
@@ -2,6 +2,7 @@ var tKey;
 var ekey;
 // getTeamScores();
 var blueKeyArray;
+var isTeam = "";
 function getTeamScores() {
   var teamScoreRequest = new XMLHttpRequest();
   teamScoreRequest.open("GET", "https://www.thebluealliance.com/api/v3/team/" + "frc1482" + "/event/" + "2019abca" + "/matches/simple" , true);
@@ -11,11 +12,17 @@ function getTeamScores() {
     var teamScoreRequestObj = JSON.parse(this.responseText);
     var j;
     for (j = 0; j < teamScoreRequestObj.length; j++) {
-      for(b = 0,) {}
-    blueKeyArray = teamScoreRequestObj[a].alliances.blue.team_keys;
-  }
-  }
-
+      blueKeyArray = teamScoreRequestObj[j].alliances.blue.team_keys;
+      for(b = 0, b < 2) {
+          if(blueKeyArray(b) = "frc1482") {
+              isTeam = "blue"
+          }
+      }
+       if(isTeam == "blue") {
+          console.log("")
+       } else {
+          console.log("")
+       }
   }
 
 }
